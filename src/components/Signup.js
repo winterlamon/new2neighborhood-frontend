6aa4df7b0caa2773abe8e3f2292facfd9d48b229
jcstorms1 <jcstorms25@gmail.com>
2018-02-12 21:58:13 -0500

    added validation for signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Col, Row, Input } from "react-materialize";
+import { Button, Row, Col, Input } from "react-materialize";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import * as actions from "../actions";
@@ -24,16 +24,50 @@ class Signup extends React.Component {
     this.setState({ fields: newField });
   };
 
+  validate = () => {
+    let messages = [];
+    if (this.state.fields.firstName === "") {
+      messages.push("• First Name can't be blank!");
+    }
+    if (this.state.fields.lastName === "") {
+      messages.push("• Last Name can't be blank!");
+    }
+    if (this.state.fields.email === "") {
+      messages.push("• Email can't be blank!");
+    }
+    if (
+      !/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+        this.state.fields.email
+      )
+    ) {
+      messages.push("• Must be a valid email!");
+    }
+    if (
+      !/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(this.state.fields.password)
+    ) {
+      messages.push(
+        "• Password must be a minimum of 8 characters in length, and contain at least one letter and number!"
+      );
+    }
+    return messages;
+  };
+
   handleSubmit = event => {
     event.preventDefault();
-    this.props.signup(this.state.fields).then(res => {
-      if (res.error) {
-        this.setState({ error: true }, swal(res.error));
-      } else {
-        this.props.history.push("/dashboard");
-        this.props.setCoords();
-      }
-    });
+    let messages = this.validate();
+    if (messages.length > 0) {
+      swal("Oh No!", messages.join("\r\n"));
+    } else {
+      console.log("will submit");
+      // this.props.signup(this.state.fields).then(res => {
+      //   if (res.error) {
+      //     this.setState({ error: true }, swal(res.error));
+      //   } else {
+      //     this.props.history.push("/dashboard");
+      //     this.props.setCoords();
+      //   }
+      // });
+    }
   };
 
   render() {
